9996a39332afcd890614cf11b6f68bbe5a2e3595
Mahmudul Alam <expelmahmud@gmail.com>
2023-07-31 16:36:55 +0600

    chore: basic scaffolding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,117 @@
-import Image from 'next/image'
+// TODO: Remove this entire file and start your own project from scratch.
+// Happy coding!
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import Link from "next/link";
+import { GithubIcon } from "lucide-react";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import ThemeToggle from "@/components/ThemeToggle";
+import { Badge } from "@/components/ui/badge";
+import { buttonVariants } from "@/components/ui/button";
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default function IndexPage() {
+	return (
+		<main className="container grid items-center gap-6 space-y-4 pb-8 pt-6 md:py-10">
+			<div className="space-y-2">
+				<h1 className="text-5xl font-bold">{siteConfig.title}</h1>
+				<p className="text-slate-500">{siteConfig.description}</p>
+				<div className="space-x-2">
+					<Link
+						className={cn(buttonVariants(), "space-x-2")}
+						href="https://github.com/A7med3bdulBaset/next-template"
+					>
+						<GithubIcon />
+						<span>Github</span>
+					</Link>
+					<ThemeToggle variant="outline" size="icon" />
+				</div>
+			</div>
+			<div className="space-y-2">
+				<h2 className="text-3xl font-semibold">Included Features:</h2>
+				<ul className="list-inside ps-8 marker:text-2xl marker:text-primary-500/50">
+					<li>Next.js 13.4: App Router and Server Actions</li>
+					<li>Typescript</li>
+					<li>Tailwind CSS</li>
+					<li>
+						<Link
+							href="https://ui.shadcn.com"
+							target="_blank"
+							className="underline decoration-sky-500 decoration-wavy underline-offset-4 hover:text-sky-500"
+						>
+							Shadcn/ui
+						</Link>{" "}
+						Beautifully and customizable components.
+					</li>
+					<li>Lucide Ycons</li>
+					<li>Next Themes: light/dark toggler</li>
+					<li>ESLint</li>
+					<li>Prettier with imports and tailwindcss classes sorting</li>
+				</ul>
+			</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+			<div className="space-y-2">
+				<h2 className="text-3xl font-semibold">Scripts:</h2>
+				<p className="text-slate-">
+					This template uses <Badge variant="secondary">pnpm</Badge> as the
+					package manager. If you want to use other package managers, you can
+					delete <Badge variant="secondary">pnpm-lock.yaml</Badge> and run{" "}
+					<Badge variant="secondary">npm install</Badge> or{" "}
+					<Badge variant="secondary">yarn install</Badge>.
+				</p>
+				<ul className="list-disc ps-8 marker:text-2xl marker:text-primary-500/50">
+					<li>
+						<Badge>dev</Badge> Start the development server
+					</li>
+					<li>
+						<Badge>build</Badge> Build for production
+					</li>
+					<li>
+						<Badge>start</Badge> Start production server
+					</li>
+					<li>
+						<Badge>preview</Badge> Build and start production server
+					</li>
+					<li>
+						<Badge>lint</Badge> Lint code
+					</li>
+					<li>
+						<Badge>format</Badge> Format code
+					</li>
+					<li>
+						<Badge>format:check</Badge> Check code formatting
+					</li>
+					<li>
+						<Badge>typecheck</Badge> Check types
+					</li>
+					<li>
+						<Badge>ci-check</Badge> Run all checks
+					</li>
+				</ul>
+			</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			<div className="space-y-2">
+				<h2 className="text-3xl font-semibold">PNPM Cheat Sheet</h2>
+				<ul className="list-disc ps-8 marker:text-2xl marker:text-primary-500/50">
+					<li>
+						<Badge>pnpm install</Badge> Install dependencies
+					</li>
+					<li>
+						<Badge>pnpm add &lt;dependency&gt;</Badge> Add dependencies (-D for
+						devDependencies)
+					</li>
+					<li>
+						<Badge>pnpm dlx &lt;package&gt;</Badge> like npx
+					</li>
+					<li>
+						<Badge>pnpm run &lt;script&gt;</Badge> Run scripts (or just{" "}
+						<Badge>pnpm &lt;script&gt;</Badge> if there is no command conflict)
+					</li>
+					<li>
+						<Badge>pnpm update</Badge> Update dependencies
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+import { default as shadcnPlugin } from "./src/lib/shadcnPlugin";
+
+const config = {
+	darkMode: ["class"],
+	content: ["./src/**/*.{ts,tsx}"],
+	plugins: [require("tailwindcss-animate"), shadcnPlugin],
+} satisfies Config;
+
+export default config;
